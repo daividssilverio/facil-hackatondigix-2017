db1da2038665eb13ac221f87c3df7046dc895c53
Daivid Santos Silverio <daividsilverio@digix.com.br>
2017-12-15 18:32:17 -0300

    layout das contas

--- src/pages/servicos/servicos.ts
+++ src/pages/servicos/servicos.ts
@@ -8,10 +8,10 @@ import {Servico} from "../../model/servico";
 export class ServicosPage {
 
   servicosDisponiveis = [
-    new Servico("Energiza", "https://robohash.org/energiza?set=set4&size=64x64"),
-    new Servico("Águas Guariroba", "https://robohash.org/aguas?set=set4&size=64x64"),
-    new Servico("Prefeitura", "https://robohash.org/prefeitura?set=set4&size=64x64"),
-    new Servico("Estado de MS", "https://robohash.org/estadoms?set=set4&size=64x64"),
+    [new Servico("Energiza", "https://robohash.org/energiza?set=set4&size=64x64"),
+      new Servico("Águas Guariroba", "https://robohash.org/aguas?set=set4&size=64x64")],
+    [new Servico("Prefeitura", "https://robohash.org/prefeitura?set=set4&size=64x64"),
+      new Servico("Estado de MS", "https://robohash.org/estadoms?set=set4&size=64x64")]
   ];
 
   constructor(public navCtrl: NavController) {

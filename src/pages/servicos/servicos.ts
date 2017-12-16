879371dab959719d1881db402a992025e6c79dae
Sandro <sandro.oyadomari@gmail.com>
2017-12-16 00:29:03 -0300

    lista de servicos

--- src/pages/servicos/servicos.ts
+++ src/pages/servicos/servicos.ts
@@ -8,24 +8,17 @@ import { ContaPage } from '../conta/conta';
   templateUrl: 'servicos.html'
 })
 export class ServicosPage {
+  
+  contas : Conta[]   = [new Conta(145.8, "Dezembro/2018", "nao-pago"),
+  new Conta(145.8, "Novembro/2018", "pago"),
+  new Conta(156.8, "Outubro/2018", "pago"),
+  new Conta(195.71, "Setembro/2018", "pago")];
 
-  servicosDisponiveis = [
-    [new Servico("Energiza", "assets/imgs/luz.png", [new Conta(145.8, "Dezembro/2018", "nao-pago"),
-      new Conta(145.8, "Novembro/2018", "pago"),
-      new Conta(156.8, "Outubro/2018", "pago"),
-      new Conta(195.71, "Setembro/2018", "pago")]),
-      new Servico("Águas Guariroba", "assets/imgs/agua.png", [new Conta(145.8, "Dezembro/2018", "nao-pago"),
-        new Conta(145.8, "Novembro/2018", "pago"),
-        new Conta(156.8, "Outubro/2018", "pago"),
-        new Conta(195.71, "Setembro/2018", "pago")])],
-    [new Servico("IPTU", "assets/imgs/iptu.png", [new Conta(145.8, "Dezembro/2018", "nao-pago"),
-      new Conta(145.8, "Novembro/2018", "pago"),
-      new Conta(156.8, "Outubro/2018", "pago"),
-      new Conta(195.71, "Setembro/2018", "pago")]),
-      new Servico("IPVA", "assets/imgs/ipva.png", [new Conta(145.8, "Dezembro/2018", "nao-pago"),
-        new Conta(145.8, "Novembro/2018", "pago"),
-        new Conta(156.8, "Outubro/2018", "pago"),
-        new Conta(195.71, "Setembro/2018", "pago")])]
+  servicosDisponiveis : any[] = [
+    [new Servico("Energiza", "assets/imgs/luz.png", this.contas),
+      new Servico("Águas Guariroba", "assets/imgs/agua.png", this.contas)],
+    [new Servico("IPTU", "assets/imgs/iptu.png", this.contas),
+      new Servico("IPVA", "assets/imgs/ipva.png", this.contas)]
   ];
 
   constructor(public navCtrl: NavController) {

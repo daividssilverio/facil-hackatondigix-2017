7c0fce076f49e26fdeb75295b956dfe3d033a44d
Sandro <sandro.oyadomari@gmail.com>
2017-12-15 23:46:30 -0300

    tela de pagamento

--- src/model/pendencia.ts
+++ src/model/pendencia.ts
@@ -1,4 +1,4 @@
 export class Pendencia {
 
-    constructor (public nomeDaEmpresa: string, public valor: Number, public emAtraso: boolean) {}
+    constructor (public nomeDaEmpresa: string, public valor: string, public emAtraso: boolean, public mesDeVencimento: string) {}
 }
